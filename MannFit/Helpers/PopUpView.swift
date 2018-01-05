930fabc1c6eccbe2e46fd7c68dbdd2c6df9c3797
Daniel Till <danieltill@Daniels-MacBook-Air.local>
2018-01-04 19:49:28 -0500

    10D_AddPopUpView

---
+++ MannFit/Helpers/PopUpView.swift
@@ -0,0 +1,21 @@
+//
+//  PopUpView.swift
+//  MannFit
+//
+//  Created by Daniel Till on 1/4/18.
+//  Copyright © 2018 MannFit Labs. All rights reserved.
+//
+
+import UIKit
+
+class PopUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
